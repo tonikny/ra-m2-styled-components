ec75f57672d9e82b60a936188eadaf5890b23c5a
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-12-18 20:49:49 +0100

    primera-revision

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 import { dimensions, FlexBox } from '../../styles'
 
+// No se donde esta el fallo, pero el borde izquierdo no aparece y el sombreado tampoco es o parece el correcto
 const Card = styled(FlexBox)`
   border-width: ${({ border }) => border || '1px'};
   border-radius: ${({ radius }) => radius || dimensions.card.radius};
-  background-color: ${({ color }) => color || ""};
+  background-color: ${({ color }) => color || ''};
 `
 
 export default Card
